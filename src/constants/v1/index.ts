cad5d21ed21361eca067f1b2a0e63ae973d135fb
futurecloudtech <81574766+futurecloudtech@users.noreply.github.com>
2021-07-11 22:59:35 +0800

    First commit

---
+++ src/constants/v1/index.ts
@@ -0,0 +1,14 @@
+import { Interface } from '@ethersproject/abi'
+import { ChainId } from '@pancakeswap-libs/sdk'
+import V1_EXCHANGE_ABI from './v1_exchange.json'
+import V1_FACTORY_ABI from './v1_factory.json'
+
+const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xca143ce32fe78f1f7019d7d551a6402fc5350c73', //TODO
+  [ChainId.BSCTESTNET]: '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+}
+
+const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
+const V1_EXCHANGE_INTERFACE = new Interface(V1_EXCHANGE_ABI)
+
+export { V1_FACTORY_ADDRESSES, V1_FACTORY_INTERFACE, V1_FACTORY_ABI, V1_EXCHANGE_INTERFACE, V1_EXCHANGE_ABI }
