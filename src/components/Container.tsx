9f88ea5cda23c33d7822b36be8900740047833ce
futurecloudtech <81574766+futurecloudtech@users.noreply.github.com>
2021-08-28 14:00:19 +0800

    update

---
+++ src/components/Container.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  width: 100%;
+  padding: 32px 16px;
+
+  background-image: url('/images/pixel/bg.png');
+  background-repeat: no-repeat;
+  background-position: bottom 24px center;
+  background-size: 90%;
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    background-size: contain, 100%, 100%;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/pixel/bg.png');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: contain, 100%, 100%;
+    min-height: 100vh;
+  }
+`
+
+export default Container
---
+++ src/components/Menu/config.ts
@@ -0,0 +1,131 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pancakeswap.finance/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    // status: {
+    //   text: 'MIGRATE',
+    //   color: 'warning',
+    // },
+    items: [
+      // {
+      //   label: 'LP Migration',
+      //   href: '/migrate',
+      // },
+      {
+        label: 'Exchange',
+        href: 'https://exchange.pancakeswap.finance/#/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      },
+      // {
+      //   label: 'V1 Liquidity (Old)',
+      //   href: '/pool',
+      // },
+    ],
+  },
+  // {
+  //   label: 'Farms',
+  //   icon: 'FarmIcon',
+  //   href: 'https://pancakeswap.finance/farms',
+  // },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: 'https://pancakeswap.finance/pools',
+  // },
+  // {
+  //   label: 'Prediction (BETA)',
+  //   icon: 'PredictionsIcon',
+  //   href: 'https://pancakeswap.finance/prediction',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://pancakeswap.finance/lottery',
+  // },
+  // {
+  //   label: 'Collectibles',
+  //   icon: 'NftIcon',
+  //   href: 'https://pancakeswap.finance/nft',
+  // },
+  // {
+  //   label: 'Team Battle',
+  //   icon: 'TeamBattleIcon',
+  //   href: 'https://pancakeswap.finance/competition',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: 'https://pancakeswap.finance/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: 'https://pancakeswap.finance/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: 'https://pancakeswap.finance/profile',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'Overview',
+  //       href: 'https://pancakeswap.info',
+  //     },
+  //     {
+  //       label: 'Tokens',
+  //       href: 'https://pancakeswap.info/tokens',
+  //     },
+  //     {
+  //       label: 'Pairs',
+  //       href: 'https://pancakeswap.info/pairs',
+  //     },
+  //     {
+  //       label: 'Accounts',
+  //       href: 'https://pancakeswap.info/accounts',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: 'https://pancakeswap.finance/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Facebook',
+        href: 'https://facebook.com',
+      },
+      {
+        label: 'Telegram',
+        href: 'https://t.me/sleepingdogecoin',
+      },
+      {
+        label: 'Twitter',
+        href: 'https://twitter.com/sleepingdogecoin',
+      }
+    ],
+  },
+]
+
+export default config
